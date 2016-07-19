f355c0fdfdb9d604f0c0916942daa283aa91d662
vaemavric <vaemavric@gmail.com>
2016-07-19 20:29:07 +0100

    Commit 2
    Added Delete student feature
    Added Reassign card feature

--- cardreader/cardreader.py
+++ cardreader/cardreader.py
@@ -13,9 +13,12 @@ import serial.tools.list_ports
 import mysql.connector
 from mysql.connector import errorcode
 import datetime
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
+from msvcrt import getch
+from msvcrt import kbhit
+from time import sleep
 today = datetime.now().date()
-tomorrow = datetime.now().date() + timedelta(days=1)
+
 #connect to mysql database
 try:
 	cnx = mysql.connector.connect(user='python', password= "python", host = '127.0.0.1', database='students')
@@ -26,67 +29,117 @@ except mysql.connector.Error as err:
 		print("Database does not exist")
 	else:
 		print(err)
-	
+
+#create cursor object		
 cursor = cnx.cursor()
+
+#queries & commands for MySQL database
 add_student = ("INSERT INTO students (Name, Card_Number, DateC, LastL) VALUES (%s, %s, %s, %s)")
-#data_student = ('James', 'hello', today, tomorrow)
-#cursor.execute(add_student, data_student)
-#cnx.commit()
+delete_student = ("DELETE FROM students WHERE Card_Number =  '{!s}' LIMIT 1")
+update_lastl = ("UPDATE students SET LastL = '{!s}' WHERE Card_Number = '{!s}'")
+reassign_card = ("UPDATE students SET Name = '{!s}' WHERE Card_Number = '{!s}'")
 
-query = ("SELECT Name, Card_Number, DateC, LastL FROM students "
+createq = ("SELECT Name, Card_Number, DateC, LastL FROM students "
 		"WHERE DateC BETWEEN %s and %s")
-cardquery = ("SELECT Name, Card_Number, DateC, LastL FROM students "
+lastlq = ("SELECT Name, Card_Number, DateC, LastL FROM students "
+		"WHERE LastL BETWEEN %s and %s")
+cardq = ("SELECT Name, Card_Number, DateC, LastL FROM students "
 			"WHERE MATCH(Card_Number) AGAINST('{!s}')")		
-#cursor.execute(query, ("2016-07-09", tomorrow))
-#for (Name, Card_Number, DateC, LastL) in cursor:
-#	print("{}, {} was created on {:%d %b %Y} and last logged in {:%d %b %Y}".format(
-#    Card_Number, Name, DateC, LastL))
 
-#ser =  serial.Serial("COM15")
 
 #code for auto selecting arduino board
 ports = list(serial.tools.list_ports.comports())
 for p in ports:
 	if "USB-SERIAL CH340" in p[1]:
 		ard = p[1][-6:-1]
-print " Arduino connected on: " + ard
-ser = serial.Serial(ard)
+
+
+try:
+	ser = serial.Serial(ard)
+	print " Arduino connected on: " + ard
+except:
+	print("Unable to connect to arduino")
+	quit()
+	
+
 
 while True:
 	try:
 		UID =  ser.readline().replace(" ", "").rstrip()
 		print "Card read! UID: " + UID
 		
-		cursor.execute(cardquery.format(UID))
+		cursor.execute(cardq.format(UID))
 		cursor.fetchall()
+		delete = False
+		reassign = False
+		if kbhit() == 1:
+				key = ord(getch())
+				if key == 100:
+					delete = True
+				elif key == 114:
+					reassign = True
+				else:
+					pass
 		if cursor.rowcount == 1:
-			cursor.execute(cardquery.format(UID))
+			cursor.execute(cardq.format(UID))
 			for (Name, Card_Number, DateC, LastL) in cursor:
-				print("{}, {} was created on {:%d %b %Y} and last logged in {:%d %b %Y}".format(Card_Number, Name, DateC, LastL))
-				print("\n")
+				print("{}, {} was created on {:%d %b %Y} and last logged in {:%d %b %Y} \n".format(Card_Number, Name, DateC, LastL))
+			try:
+				cursor.execute(update_lastl.format(today, UID))
+				cnx.commit()
+			except:
+				cnx.rollback()
+			if delete == True:
+				d = raw_input("are you sure you want to delete? (Y/N) \n")
+				if d.lower() == 'y':
+					try:
+						cursor.execute(delete_student.format(UID))
+						cnx.commit()
+						print("Student deleted")
+					except:
+						cnx.rollback()
+				else:
+					pass
+			elif reassign == True:
+				new_name = raw_input("Please enter new name: \n")
+				try:
+					cursor.execute(reassign_card.format(new_name, UID))
+					cnx.commit()
+					print("Updated name")
+				except:
+					cnx.rollback()
+			else:
+				pass
+			
 		else:
-			print "Card not recorded"
-			add = raw_input('Would you like to add new card? (y/n)\n')
-			if add == 'y':
+			print "Card not in database"
+			add = raw_input('Would you like to add new card? (Y/N)\n')
+			if add.lower() == 'y':
 				name =  raw_input('Enter Name:\n')
 				data_student = (name, UID, today, today)
 				try:
 					cursor.execute(add_student, data_student)
 					cnx.commit()
 				except:
-					cursor.rollback()
+					cnx.rollback()
 				print 'User added'
 			else:
 				pass
-			print("\n")
+		
 	except(KeyboardInterrupt, SystemExit):
 		print"Quitting cardreader.py..."
 		print"Closing cursor"
 		cursor.close()
+		cnx.rollback()
 		print"Closing Database connection"
 		cnx.close()
 		print"Quitting"
 		quit()
+print"Quitting cardreader.py..."
+print"Closing cursor"
 cursor.close()
+cnx.rollback()
+print"Closing Database connection"
 cnx.close()
+print"Quitting"
 quit()
